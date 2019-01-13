28a0f01f7f9aa8e9423be018001e0d9d6b05cb4c
Armin Saeedi <saidi.armin@gmail.com>
2019-01-13 20:41:20 +0800

    Adding behaviour

--- FilesOrganizer/Form1.Designer.cs
+++ FilesOrganizer/Form1.Designer.cs
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSourceFolder = new System.Windows.Forms.TextBox();
+            this.btnOrganizer = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.lblSuccess = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose a folder to organize photos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtSourceFolder
+            // 
+            this.txtSourceFolder.Location = new System.Drawing.Point(48, 98);
+            this.txtSourceFolder.Name = "txtSourceFolder";
+            this.txtSourceFolder.ReadOnly = true;
+            this.txtSourceFolder.Size = new System.Drawing.Size(862, 31);
+            this.txtSourceFolder.TabIndex = 1;
+            this.txtSourceFolder.TabStop = false;
+            // 
+            // btnOrganizer
+            // 
+            this.btnOrganizer.Location = new System.Drawing.Point(824, 333);
+            this.btnOrganizer.Name = "btnOrganizer";
+            this.btnOrganizer.Size = new System.Drawing.Size(269, 83);
+            this.btnOrganizer.TabIndex = 3;
+            this.btnOrganizer.Text = "Organize";
+            this.btnOrganizer.UseVisualStyleBackColor = true;
+            this.btnOrganizer.Click += new System.EventHandler(this.btnOrganizer_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(48, 277);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 31);
+            this.lblError.TabIndex = 6;
+            this.lblError.Visible = false;
+            // 
+            // lblSuccess
+            // 
+            this.lblSuccess.AutoSize = true;
+            this.lblSuccess.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSuccess.ForeColor = System.Drawing.Color.Green;
+            this.lblSuccess.Location = new System.Drawing.Point(48, 152);
+            this.lblSuccess.Name = "lblSuccess";
+            this.lblSuccess.Size = new System.Drawing.Size(0, 31);
+            this.lblSuccess.TabIndex = 5;
+            this.lblSuccess.Visible = false;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(522, 333);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(269, 83);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset Form";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(926, 98);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(167, 40);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1137, 450);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblSuccess);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnOrganizer);
+            this.Controls.Add(this.txtSourceFolder);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Organizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSourceFolder;
+        private System.Windows.Forms.Button btnOrganizer;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label lblSuccess;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnBrowse;
     }
 }
 
--- FilesOrganizer/Form1.cs
+++ FilesOrganizer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,90 @@ namespace FilesOrganizer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnOrganizer_Click(object sender, EventArgs e)
+        {
+            var folderPath = txtSourceFolder.Text;
+            HideLabels();
+
+            if (!Directory.Exists(folderPath))
+            {
+                ShowError("Image folder path is not valid!");
+                return;
+            }
+            
+            try
+            {
+                foreach (var file in Directory.GetFiles(folderPath, "*.JPG"))
+                {
+                    var modifiedDate = File.GetLastWriteTime(file).ToString("yyyy_MM_dd");
+                    var destinationFolderPath = $"{folderPath}\\{modifiedDate}\\";
+                    var destinationFilePath = destinationFolderPath + file.Substring(file.LastIndexOf("\\") + 1);
+
+                    if (!Directory.Exists(destinationFolderPath))
+                        Directory.CreateDirectory(destinationFolderPath);
+
+                    if (!File.Exists(destinationFilePath))
+                        File.Move(file, destinationFilePath);
+                }
+
+                ShowSuccess("Your files successfully moved to the folders :)");
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+                return;
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtSourceFolder.Text = String.Empty;
+        }
+
+        private void HideLabels()
+        {
+            lblSuccess.Visible = false;
+            lblError.Visible = false;
+        }
+
+        private void ShowError(string errorMsg)
+        {
+            lblError.Text = errorMsg;
+            lblError.Visible = true;
+            ClearForm();
+        }
+
+        private void ShowSuccess(string successMsg)
+        {
+            lblSuccess.Text = successMsg;
+            lblSuccess.Visible = true;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+            HideLabels();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtSourceFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
